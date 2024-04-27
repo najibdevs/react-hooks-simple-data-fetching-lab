da51376a4bfc63fc1267fb5464fd53f80bf66b3b
najibdevs <najibsoftwareeng@gmail.com>
2024-04-27 15:18:24 +0300

    react hooks simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [pic, dog] = useState();
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        dog(data.message);
+      });
+  }, []);
+
+
+  if (!pic) return <p>Loading...</p>;
+
+  return <img src={pic} alt="A Random Dog" />;
+}
+
+export default App;
